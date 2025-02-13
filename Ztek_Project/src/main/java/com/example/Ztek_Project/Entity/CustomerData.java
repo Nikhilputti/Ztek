2cfc1305d436c136d00f795897159994c1f667af
Nikhilputti <85381856+Nikhilputti@users.noreply.github.com>
2025-02-13 13:31:49 +0530

    Update CustomerData.java

--- Ztek_Project/src/main/java/com/example/Ztek_Project/Entity/CustomerData.java
+++ Ztek_Project/src/main/java/com/example/Ztek_Project/Entity/CustomerData.java
@@ -4,11 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 import java.time.LocalDate;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor      
 public class CustomerData {
 
     @Id
@@ -18,36 +24,6 @@ public class CustomerData {
     private int amount;
     private LocalDate date;
 
-    public Long getId() {
-        return id;
-    }
+   
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(Long customerId) {
-        this.customerId = customerId;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    public void setAmount(int amount) {
-        this.amount = amount;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
 }
-
